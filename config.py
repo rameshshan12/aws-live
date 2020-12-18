64de5c3a8d699a9c7a83f98362fca1328c5902bf
rameshshan12 <76172356+rameshshan12@users.noreply.github.com>
2020-12-19 01:19:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cltfpl7h3rqw.us-east-2.rds.amazonaws.com"
-customuser = "ramesh"
-custompass = "ramesh12"
-customdb = "employee"
-custombucket = "employee-new"
-customregion = "us-east-2"
-
